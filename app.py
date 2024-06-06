f86fc0935eab08621af3900d8bf563681a8c02ee
Sovann Vin <151512736+error404100230@users.noreply.github.com>
2024-06-06 19:36:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Docker with Python !'
+    return 'Hello, Docker with Python !. happy coding ^^'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
